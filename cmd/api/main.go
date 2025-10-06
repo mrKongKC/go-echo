341815482bcd0e26de157acd2dca5de2dfa181f5
Passakorn PJ <passakornpj@MacBook-Air-khxng-Passakorn.local>
2025-10-06 21:58:44 +0700

    setup port

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	err := godotenv.Load()
+	port := os.Getenv("APP_PORT")
+	appAddress := fmt.Sprintf("localhost:%s", port)
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Hello, Echo 👋")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+
+	e.Logger.Fatal(e.Start(appAddress))
 }
